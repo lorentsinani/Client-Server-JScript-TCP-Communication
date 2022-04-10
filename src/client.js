af7bb6101df46b91a5e8d264f76f4d39136a414f
Loreta Basha <97306481+LoretaBashaa@users.noreply.github.com>
2022-04-10 19:14:25 +0300

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,1 +1,28 @@
+// A client for the capitalization server. After connecting, every line 
+
+// sent to the server will come back capitalized. 
+
+// 
+
+// Use interactively: 
+
+// 
+
+// node capitalizeclient.js 10.0.1.40 
+
+// 
+
+// Or pipe in a file to be capitalized: 
+
+// 
+
+// node capitalizeclient.js 10.0.1.40 < myfile 
+
+ 
+
+const net = require('net'); 
+
+const readline = require('readline'); 
+
+ 
 
