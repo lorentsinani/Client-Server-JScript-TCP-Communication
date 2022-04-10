53d5574e2f0e6eb467ea69ba747c992aac0ccce6
Lorent Sinani <lorent.sinani0@gmail.com>
2022-04-10 17:47:19 +0200

    creating server

--- src/sever.js
+++ src/sever.js
@@ -1,1 +1,12 @@
+const net = require('net');
+const fs = require('fs');
+
+//server creation and message from which address and port is connection made
+const server = net.createServer((socket) => {
+    console.log(
+        'Connection from',
+        socket.remoteAddress,
+        'port',
+        socket.remotePort
+    );
 
