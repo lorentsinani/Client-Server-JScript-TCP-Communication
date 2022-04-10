abcfe43d95297ef106e81af0d486689b48cff1cf
etrordermaku1 <76623917+etrordermaku1@users.noreply.github.com>
2022-04-10 21:27:00 +0300

    Update client.js

--- src/client.js
+++ src/client.js
@@ -23,6 +23,31 @@
 const net = require('net'); 
 
 const readline = require('readline'); 
+client.on('data', (data) => { 
+
+console.log(data.toString('utf-8')); 
+
+}); 
+
+ 
+
+const rl = readline.createInterface({ 
+
+input: process.stdin 
+
+}); 
+
+rl.on('line', (line) => { 
+
+client.write(${line}\n); 
+
+}); 
+
+rl.on('close', () => { 
+
+client.end(); 
+
+});
 
  
 
