ba7f4309737c5bc6e25150e72a39c03e5b334680
Lorent Sinani <lorent.sinani0@gmail.com>
2022-04-10 18:46:34 +0200

    created socket with data from client

--- src/sever.js
+++ src/sever.js
@@ -10,3 +10,17 @@ const server = net.createServer((socket) => {
         socket.remotePort
     );
 
+//beginning of getting data from client
+    socket.on('data', (buffer) => {
+        console.log(
+            'Request from',
+            socket.remoteAddress,
+            'port',
+            socket.remotePort
+        );
+        // showing input of client at server side as a string
+        console.log(buffer.toString().slice(''));
+        //converting input to string without spaces and inicialize that on a vriable called message
+        let message = buffer.toString().trim();
+        //showing at server side the length of data inputed
+        console.log("Request string length is: " + message.length);
