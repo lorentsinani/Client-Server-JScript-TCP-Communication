916021bfb47848e4177fff487b7e6397c735a841
Lorent Sinani <lorent.sinani0@gmail.com>
2022-04-11 17:16:27 +0200

    inserted player class and end of code

--- src/tictactoeserver.js
+++ src/tictactoeserver.js
@@ -75,3 +75,48 @@ class Game {
         this.currentPlayer = this.currentPlayer.opponent;
     }
 }
+
+class Player {
+    constructor(game, socket, mark) {
+        Object.assign(this, { game, socket, mark });
+        this.send(`WELCOME ${mark}`);
+        if (mark === 'X') {
+            game.currentPlayer = this;
+            this.send('MESSAGE Waiting for opponent to connect');
+        } else {
+            this.opponent = game.playerX;
+            this.opponent.opponent = this;
+            this.opponent.send('MESSAGE Your move');
+        }
+
+        socket.on('data', (buffer) => {
+            const command = buffer.toString('utf-8').trim();
+            if (command === 'QUIT') {
+                socket.destroy();
+            } else if (/^MOVE \d+$/.test(command)) {
+                const location = Number(command.substring(5));
+                try {
+                    game.move(location, this);
+                    this.send('VALID_MOVE');
+                    this.opponent.send(`OPPONENT_MOVED ${location}`);
+                    if (this.game.hasWinner()) {
+                        this.send('VICTORY');
+                        this.opponent.send('DEFEAT');
+                    } else if (this.game.boardFilledUp()) {
+                        [this, this.opponent].forEach(p => p.send('TIE'));
+                    }
+                } catch (e) {
+                    this.send(`MESSAGE ${e.message}`);
+                }
+            }
+        });
+
+        socket.on('close', () => {
+            try { this.opponent.send('OTHER_PLAYER_LEFT'); } catch (e) { }
+        });
+    }
+
+    send(message) {
+        this.socket.write(`${message}\n`);
+    }
+}
